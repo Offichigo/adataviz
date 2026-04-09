84c928ba75918ad252c991461633b6974c49a2d9
Offichigo <oph.perianin@gmail.com>
2026-04-09 22:32:51 +0200

    correction du js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import "./style.css";
 import { requestAPI } from "./data";
 import { extractCitiesFromData } from "./view";
 // const data = await requestAPI();
-// extractCitiesFromData();
+extractCitiesFromData();
