1f6fa3383eded01168e13871ca4398e20e1afe7b
Offichigo <oph.perianin@gmail.com>
2026-04-09 11:21:02 +0200

    ajout fonction createcard details

--- src/view.js
+++ src/view.js
@@ -3,14 +3,14 @@ import { requestAPI } from "./data";
  *
  * @param {object} result toilette
  */
-
+//**transformation
+// ajout boolean pmr */
 export const createCards = (result) => {
   const section = document.querySelector(".card-toilets");
   //création nouvel élement
   //création numero de carte
   const cards = document.createElement("div");
   cards.classList.add("card-toilet");
-
   const num = String(result.gid).padStart(3, "0");
 
   cards.innerHTML = `
@@ -21,11 +21,43 @@ export const createCards = (result) => {
   section.appendChild(cards);
 };
 
+export const createCardsDetails = (result) => {
+  const section = document.querySelector(".card-toilets");
+  const cardsDetails = document.createElement("div");
+  cards.classList.add("card-toilet-details");
+  const num = String(result.gid).padStart(3, "0");
+
+  cardsDetails.innerHTML = `
+        <span class="card-num">n°${num}</span>
+    <h3 class="name-card">${result.nom ?? "Nom inconnue"}</h3>
+    <p>Apparais dans le quartier : ${result.quartier ?? "Pas renseigné"}
+    <ul>
+    <li>${result.pole ?? "Pôle inconnue"}</li>
+    <li>${result.configuration_wc ?? "Configuration wc inconnue"}</li>
+    <li>${result.accessibilite_pmr ?? "✗"}</li>
+    <li>${result.etat ?? "Pas plus d'informations"}</li>
+    <li>${result.horaire_ouverture ?? "Pas plus d'informations"}</li>
+    <li>${result.ouverture ?? "Pas plus d'informations"}</li>
+    <li>${result.equipement_table_langer ?? "Pas plus d'informations"}</li>
+    <li>${result.equipement_urinoir ?? "Pas plus d'informations"}</li>
+    <li>${result.geo_point_2d ?? "Pas plus d'informations"}</li>
+
+    }
+  `;
+  section.appendChild(cardsDetails);
+};
+
+/**
+ * * ecoute du clique
+ * ajout carte detaillé
+ * ajout au clique changement de la carte
+ * crée card qui affiche tous les autres détail et fait une comparaison et donne le type en fonction du type de wc et de son equipement
+ *  clique affiche les détails pour chaque card pour en savoir plus
+ * adresse - commune - quartier- type equip - acces pmr (true false)-hour-état=boolean
+ *
+ */
 /**
  * récupérer les données toilette publics
- * crée card qui affiche les commune et le quartier
- * clique affiche les détails pour chaque card pour en savoir plus
- * adresse - commune - quartier- type equip - acces pmr (true false)-hour-état=boolean
  *affiner ce que donne le donne le tableau
  */
 export const extractCitiesFromData = async () => {
